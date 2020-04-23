13af9d003f264071bf96bd552d4f7c9292dcc9b5
John Williams <john.yong.williams@gmail.com>
2020-04-23 19:11:20 -0400

    Updates

---
+++ precache-manifest.a37cf5bc25bc0caadd325aef69980733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641fefd742184d8e62774a29898d8d95",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "3aaa58aa9b1d398876d6",
+    "url": "/random-quote-machine/static/css/main.cbe02afe.chunk.css"
+  },
+  {
+    "revision": "2aa9dcb7f2286dca2c54",
+    "url": "/random-quote-machine/static/js/2.45db90eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.45db90eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aaa58aa9b1d398876d6",
+    "url": "/random-quote-machine/static/js/main.5f113b89.chunk.js"
+  },
+  {
+    "revision": "52b3aacfd4b50869dda1",
+    "url": "/random-quote-machine/static/js/runtime-main.116f0ca4.js"
+  }
+]);
--- src/redux/actionTypes.js
+++
@@ -1,3 +0,0 @@
-// export action types for use with action creators ect.
-export const GET_NEW_QUOTE = "GET_NEW_QUOTE";
-export const TWEET_QUOTE = "TWEET_QUOTE";
